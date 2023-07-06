65a75a88cd4093dfa13cc72312003445e13e6c63
imkozin <ivan.kozin@hotmail.com>
2023-07-06 17:22:03 +0300

    Added Authentication

--- travelmap_top/core/urls.py
+++ travelmap_top/core/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from .views import add_country, bulk_insert_countries, countries, bulk_insert, CountryAPIList
+from django.urls import path, include
+from .views import CountryAPIList, CountryAPIUpdate, CountryAPIDestroy
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('site-auth', include('rest_framework.urls')),
     path('api/countries/', CountryAPIList.as_view()),
-    # path('api/country/<int:pk>', CountryAPIUpdate.as_view()),
+    path('api/countries/<int:pk>', CountryAPIUpdate.as_view()),
+    path('api/countries/delete/<int:pk>', CountryAPIDestroy.as_view())
     # path('api/country/add', add_country, name='add-country'),
-    path('bulk-insert/', bulk_insert, name='bulk-insert'),
-    # path('api/countries/', countries, name='countries')
+    # path('api/countries/', countries, name='countries'),
+    # path('bulk-insert/', bulk_insert, name='bulk-insert'),
 ]
